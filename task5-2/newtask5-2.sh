569e9ac60d015e6eef4c7d954c742217fba6af61
KEERTHANA <keerthanaks1814@gmail.com>
2024-06-06 15:02:10 +0000

    task5-2

---
+++ task5-2/newtask5-2.sh
@@ -0,0 +1,10 @@
+This is the first line 
+Guvi welcomes you
+Im learning guvi
+Please give us your attention
+This task will give you practice
+Feel welcome and leaning your best
+Let us give our best
+Guvi give you best placement 
+
+
